b356ac15c2743aa04826dc0871567b40e0dfc54e
AliMousa27 <jafar.myhero@gmail.com>
2024-02-18 22:05:05 +0100

    finished using tiktok API to get mp3 file of tts

--- shorts.py
+++ shorts.py
@@ -1,9 +1,9 @@
-
-
 from moviepy.video.tools.subtitles import SubtitlesClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.editor import TextClip, CompositeVideoClip
 import random
+from TikTokTTS.main import tts
+
 subs = [((0, 1), "This"),
         ((1,2), "is"),
         ((2,3), "a"),
@@ -24,9 +24,17 @@ def create_subtitle(subs):
     generator = lambda txt: TextClip(txt, font='Arial-Bold', fontsize=100, color='white')
     return SubtitlesClip(subs, generator).set_position(('center'))
 
-clip = cut_video('min.mp4')
-subtitles = create_subtitle(subs)
-final = CompositeVideoClip([clip, subtitles])
-final.write_videofile("short.mp4")
-final.close()
+def main():
+    text = "this is just a whole lot of text that is going to be converted to audio"
+    voice = "en_us_006"
+    #TODO make env var
+    session_id = "d005dc431f90cf9f45a5ed5e54d4afed"
+    tts(session_id, voice, text, "voice.mp3")
+    '''	clip = cut_video('min.mp4')
+    subtitles = create_subtitle(subs)
+    final = CompositeVideoClip([clip, subtitles])
+    final.write_videofile("short.mp4")
+    final.close()'''
 
+
+if __name__ == "__main__": main()
