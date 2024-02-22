246c11d62ca9f5a9f962efa96b5311ab1fd6fe18
AliMousa27 <jafar.myhero@gmail.com>
2024-02-22 21:52:20 +0100

    fixed possible typos caused by AI and changes resoltuion to fit shorts

--- shorts.py
+++ shorts.py
@@ -17,15 +17,19 @@ def cut_video(video_path, video_length):
         clip = clip.subclip(random_point, random_point+video_length)
         return clip
 
-def transcribe_audio(audio_path):
+def transcribe_audio(audio_path,txt):
     model = whisper.load_model("base")
     #transcribe the audio and get the word timestamps
     result = model.transcribe(audio=audio_path,word_timestamps=True)
     segments = result['segments']
     subs = []
+    split_text = txt.split(' ')
+    i = 0
+    print(f"split_text: {split_text}")
     for segment in segments:
         for words in segment["words"]:
-            subs.append(((words["start"], words["end"]), words["word"]))
+            subs.append(((words["start"], words["end"]), split_text[i]))
+            i+=1
     
     return subs
     
@@ -35,9 +39,18 @@ def get_text(file_path):
 
 
 def combine_and_write(clip, subtitles, audioclip, output_path):
-    final = CompositeVideoClip([clip, subtitles])
+    #crop the video to 9:16 aspect ratio as 9:16 is the ratio for width to height in tiktok
+    crop_width = clip.h * (9 / 16)
+    crop_height = crop_width * (16 / 9)
+    #center the crop
+    clip_cropped = clip.crop(x_center=clip.w/2, y_center=clip.h/2, width=int(crop_width), height=int(crop_height))
+    #resize the video to height of 1280 while maintiang aspect
+    clip_resized = clip_cropped.resize(height=1280) 
+       
+    final = CompositeVideoClip([clip_resized, subtitles])
     final = final.set_audio(audioclip)
     final.write_videofile(output_path)
+
     final.close()
 
 def create_subtitle(subs):
@@ -56,9 +69,8 @@ def main():
     
     tts(SESSION_ID, VOICE, TEXT, AUDIO_FILE_PATH)
     audioclip = AudioFileClip(AUDIO_FILE_PATH)
-    subtitles = create_subtitle(transcribe_audio(AUDIO_FILE_PATH))
+    subtitles = create_subtitle(transcribe_audio(AUDIO_FILE_PATH,TEXT))
     clip = cut_video('Videos/min.mp4',audioclip.duration)
     combine_and_write(clip, subtitles, audioclip, "Videos/short.mp4")
 
 if __name__ == "__main__": main()
-
